9a85416dc5b77792378b8407d53f80fd38763d3c
Er-Sairaj-Thoart <sairajthorat1221@gmail.com>
2026-01-19 22:27:38 +0530

    Init: Assignment 1

---
+++ Assignment-1/q1.py
@@ -0,0 +1,9 @@
+# q1.py
+
+"""
+Question: Define a class which has at least two methods: getString: to get a string from console input printString: to print the string in upper case. Also please include simple test function to test the class methods.
+
+Hints: Use init method to construct some parameters
+"""
+
+# Start coding below...
